21e2097cf8b04e56a104a5e54d50840b5ba87644
영진 <yeongjin@yeongjin-ui-MacBookPro.local>
2024-07-10 17:40:03 +0900

    init

---
+++ BackEnd/MadeleineLove/src/main/java/sideproject/madeleinelove/MadeleineLoveApplication.java
@@ -0,0 +1,13 @@
+package sideproject.madeleinelove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MadeleineLoveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MadeleineLoveApplication.class, args);
+	}
+
+}
---
+++ BackEnd/MadeleineLove/src/test/java/sideproject/madeleinelove/MadeleineLoveApplicationTests.java
@@ -0,0 +1,13 @@
+package sideproject.madeleinelove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MadeleineLoveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
