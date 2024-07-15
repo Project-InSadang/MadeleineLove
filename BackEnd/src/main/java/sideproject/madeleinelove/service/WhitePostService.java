2d0ec8f4e5a3225781736ffadadaf2e5bfab759a
susooo <92291198+susooo@users.noreply.github.com>
2024-07-15 10:45:12 +0900

    fix: null content, null method exception

--- BackEnd/src/main/java/sideproject/madeleinelove/service/WhitePostService.java
+++ BackEnd/src/main/java/sideproject/madeleinelove/service/WhitePostService.java
@@ -16,5 +16,40 @@ public class WhitePostService {
     private WhitePostRepository whitePostRepository;
     private static final AtomicInteger ID_GENERATOR = new AtomicInteger(1);
 
+    public WhitePost saveWhitePost(String userId, @Valid WhiteRequestDto whiteRequestDto) {
+        validateWhiteRequestDto(whiteRequestDto);
 
+        WhitePost whitePost = createWhitePost(userId, whiteRequestDto);
+
+        return whitePostRepository.save(whitePost);
+    }
+
+    private void validateWhiteRequestDto(WhiteRequestDto whiteRequestDto) {
+        if (whiteRequestDto.getNickName() == null) {
+            whiteRequestDto.setNickName("레니");
+        } else if (whiteRequestDto.getNickName().length() > 20) {
+            throw new IllegalArgumentException("닉네임은 20자 이하이어야 합니다.");
+        }
+
+        if (whiteRequestDto.getContent() == null) {
+            throw new IllegalArgumentException("내용 작성은 필수입니다.");
+        } else if (whiteRequestDto.getContent().length() > 500) {
+            throw new IllegalArgumentException("내용은 500자 이하이어야 합니다.");
+        }
+
+        if (whiteRequestDto.getFillMethod() == null) {
+            throw new IllegalArgumentException("채우기 방법은 필수입니다.");
+        }
+    }
+
+    private WhitePost createWhitePost(String userId, WhiteRequestDto whiteRequestDto) {
+        WhitePost whitePost = new WhitePost();
+        whitePost.setUserId(userId);
+        whitePost.setPostId(ID_GENERATOR.getAndIncrement());
+        whitePost.setNickName(whiteRequestDto.getNickName());
+        whitePost.setContent(whiteRequestDto.getContent());
+        whitePost.setFillMethod(whiteRequestDto.getFillMethod());
+        whitePost.setLikeCount(0);
+        return whitePost;
+    }
 }
