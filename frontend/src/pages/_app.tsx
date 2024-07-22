e79d16be5be2efb72260fcdc0a65aff8f7e9d292
PangKiYeon <pdrkeamy@gmail.com>
2024-07-23 00:03:16 +0900

    FEAT : Hanna font 적용

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,11 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import { hanna } from '@/styles/font';
 
 export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+        <main className={`${hanna.variable} font-hanna`}>
+            <Component {...pageProps} />
+        </main>
+    );
 }
---
+++ frontend/src/styles/font.ts
@@ -0,0 +1,6 @@
+import localFont from 'next/font/local';
+
+export const hanna = localFont({
+    src: '../public/font/BMHANNA_11yrs_otf.otf',
+    variable: '--font-hanna',
+});
