5ff251d2718c9682caa09fc4c673dcbff282692c
PangKiYeon <pdrkeamy@gmail.com>
2024-07-24 12:31:23 +0900

    FEAT : welcome 화면 구현

--- frontend/src/components/background/OneCircle.tsx
+++ frontend/src/components/background/OneCircle.tsx
@@ -2,12 +2,12 @@ import FlexBox from '../layout/FlexBox';
 
 export default function OneCircle() {
     return (
-        <FlexBox className="justify-center h-screen">
+        <FlexBox className="justify-center w-full">
             <div
                 style={{
                     width: 300,
                     height: 650,
-                    background: 'rgba(58, 37, 185, 15%)',
+                    background: 'rgba(58, 37, 185, 32%)',
                     filter: 'blur(28px)',
                     borderRadius: '50%',
                 }}
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,7 +1,48 @@
 import OneCircle from '@/components/background/OneCircle';
-import TwoCircle from '@/components/background/TwoCircle';
-import CompleteButton from '@/components/button/CompleteButton';
+import FlexBox from '@/components/layout/FlexBox';
+import letterheart from '@/public/icon/heart/letter_heart.svg';
+import Image from 'next/image';
+import logo from '@/public/icon/logo.svg';
+import WelcomeButton from '@/components/button/WelcomeButton';
+import naver from '@/public/icon/naver.svg';
+import talkheart from '@/public/icon/heart/talk_heart.svg';
 
-export default function Home() {
-    return <></>;
+export default function Welcome() {
+    return (
+        <>
+            <div className="flex absolute z-0 w-screen h-screen">
+                <OneCircle />
+            </div>
+            <div className="relative z-10 w-screen h-screen">
+                <FlexBox
+                    direction="col"
+                    className="justify-center w-full h-full gap-12"
+                >
+                    <Image
+                        src={letterheart}
+                        alt="welcomeheart"
+                        width={160}
+                        height={160}
+                    />
+                    <Image src={logo} alt="logotypo" width={270} height={270} />
+                    <FlexBox direction="col" className="gap-4 mt-5">
+                        <WelcomeButton
+                            iconSrc={naver}
+                            description={'네이버 로그인'}
+                            className="bg-white text-[#767678]"
+                        />
+                        <WelcomeButton
+                            iconSrc={talkheart}
+                            description={'하트 구경하기'}
+                            className="bg-[#866BDE] text-white"
+                            iconSz={28}
+                        />
+                    </FlexBox>
+                </FlexBox>
+            </div>
+        </>
+    );
 }
+
+//logo 화질 개선
+// 버튼 onClick
