54c4da62a3bc66a88dc5361965a389fecb6116ff
susooo <92291198+susooo@users.noreply.github.com>
2024-11-16 23:17:43 +0900

    [refactor]: accessToken 반환 방식 변경

--- BackEnd/src/main/java/sideproject/madeleinelove/service/TokenServiceImpl.java
+++ BackEnd/src/main/java/sideproject/madeleinelove/service/TokenServiceImpl.java
@@ -6,15 +6,19 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import sideproject.madeleinelove.auth.CookieUtil;
 import sideproject.madeleinelove.auth.JwtUtil;
+import sideproject.madeleinelove.dto.TokenDTO;
 import sideproject.madeleinelove.entity.RefreshToken;
 import sideproject.madeleinelove.exception.TokenErrorResult;
 import sideproject.madeleinelove.exception.TokenException;
 import sideproject.madeleinelove.repository.RefreshTokenRepository;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class TokenServiceImpl implements TokenService {
     @Value("${jwt.access-token.expiration-time}")
@@ -28,14 +32,22 @@ public class TokenServiceImpl implements TokenService {
     private final CookieUtil cookieUtil;
 
     @Override
-    public void reissueAccessToken(HttpServletRequest request, HttpServletResponse response) {
-        Cookie cookie = cookieUtil.getCookie(request, "refresh_token");
+    public TokenDTO.TokenResponse reissueAccessToken(HttpServletRequest request, HttpServletResponse response) {
+        Cookie cookie = cookieUtil.getCookie(request);
         String refreshToken = cookie.getValue();
-        ObjectId userId = new ObjectId(jwtUtil.getUserIdFromToken(refreshToken));
-        RefreshToken existRefreshToken = refreshTokenRepository.findByUserId(userId);
+
+        String userIdString = jwtUtil.getUserIdFromToken(refreshToken);
+
+        if (userIdString == null || userIdString.length() != 24 || !userIdString.matches("[0-9a-fA-F]{24}")) {
+            throw new IllegalArgumentException("Invalid userId format");
+        }
+        ObjectId userId = new ObjectId(userIdString);
+
+        RefreshToken existRefreshToken = refreshTokenRepository.findByUserId(userId)
+                .orElseThrow(() -> new TokenException(TokenErrorResult.REFRESH_TOKEN_NOT_FOUND));
         String newAccessToken;
 
-        if (!existRefreshToken.getToken().equals(refreshToken) || jwtUtil.isTokenExpired(refreshToken)) {
+        if (!existRefreshToken.getRefreshToken().equals(refreshToken) || jwtUtil.isTokenExpired(refreshToken)) {
             // 리프레쉬 토큰이 다르거나, 만료된 경우
             throw new TokenException(TokenErrorResult.INVALID_REFRESH_TOKEN); // 401 에러를 던져 재로그인을 요청
         } else {
@@ -43,11 +55,18 @@ public class TokenServiceImpl implements TokenService {
             newAccessToken = jwtUtil.generateAccessToken(userId, ACCESS_TOKEN_EXPIRATION_TIME);
         }
 
-        Cookie accessTokenCookie = cookieUtil.createAccessTokenCookie(newAccessToken, ACCESS_TOKEN_EXPIRATION_TIME);
-        response.addCookie(accessTokenCookie);
-
-        Cookie newCookie = cookieUtil.createFreshTokenCookie(userId, REFRESH_TOKEN_EXPIRATION_TIME);
+        // 리프레쉬 토큰이 담긴 쿠키 생성 후 설정
+        ResponseCookie newCookie = cookieUtil.createCookie(userId, REFRESH_TOKEN_EXPIRATION_TIME);
         response.addHeader("Set-Cookie", newCookie.toString());
 
+        // 새로운 리프레쉬 토큰 DB 저장
+        RefreshToken newRefreshToken = RefreshToken.builder()
+                .userId(userId)
+                .refreshToken(newCookie.getValue())
+                .build();
+        refreshTokenRepository.save(newRefreshToken);
+
+        // 새로운 액세스 토큰을 담아 반환
+        return TokenDTO.TokenResponse.of(newAccessToken);
     }
 }
