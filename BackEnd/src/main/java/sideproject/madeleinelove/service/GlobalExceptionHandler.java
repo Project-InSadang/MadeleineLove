9b79e5a2d70549223648ac371571c942719be675
susooo <92291198+susooo@users.noreply.github.com>
2024-11-16 23:07:20 +0900

    [feat]: 헤더 관련 에러 처리 추가

--- BackEnd/src/main/java/sideproject/madeleinelove/service/GlobalExceptionHandler.java
+++ BackEnd/src/main/java/sideproject/madeleinelove/service/GlobalExceptionHandler.java
@@ -1,7 +1,9 @@
 package sideproject.madeleinelove.service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -16,15 +18,22 @@ import sideproject.madeleinelove.exception.UserException;
 @Slf4j
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
+    // Token
     @ExceptionHandler(TokenException.class)
     public ResponseEntity<ApiResponse<BaseErrorCode>> handleTokenException(TokenException e) {
         TokenErrorResult errorResult = e.getTokenErrorResult();
         return ApiResponse.onFailure(errorResult);
     }
-
+    // User
     @ExceptionHandler(UserException.class)
     public ResponseEntity<ApiResponse<BaseErrorCode>> handleUserException(UserException e) {
         UserErrorResult errorResult = e.getUserErrorResult();
         return ApiResponse.onFailure(errorResult);
+    }
+    // Header
+    @ExceptionHandler(MissingRequestHeaderException.class)
+    public ResponseEntity<String> handleMissingHeaderException(MissingRequestHeaderException ex) {
+        String errorMessage = "Required header '" + ex.getHeaderName() + "' is missing";
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errorMessage);
     }
 }
