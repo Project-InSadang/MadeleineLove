6acf2ba7d83cfa2ffb526ef25078e51344191bf6
susooo <92291198+susooo@users.noreply.github.com>
2024-11-16 22:25:20 +0900

    [refactor] : 액세스 토큰 제거 및 쿠키 설정 방식 변경

--- BackEnd/src/main/java/sideproject/madeleinelove/auth/CookieUtil.java
+++ BackEnd/src/main/java/sideproject/madeleinelove/auth/CookieUtil.java
@@ -4,48 +4,47 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.bson.types.ObjectId;
+import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class CookieUtil {
     private final JwtUtil jwtUtil;
-    private final String FRESH_TOKEN_COOKIE = "refresh_token";
-    private final String ACCESS_TOKEN_COOKIE = "access_token";
-
-    // 액세스 토큰이 담긴 쿠키를 생성하는 메서드
-    public Cookie createAccessTokenCookie(String accessToken, long expirationMillis) {
-        Cookie cookie = new Cookie(ACCESS_TOKEN_COOKIE, accessToken);
-        // 쿠키 속성 설정
-        cookie.setHttpOnly(true); // JavaScript 접근 방지
-        cookie.setSecure(true);   // HTTPS에서만 전송
-        cookie.setPath("/");      // 모든 경로에서 쿠키 접근 가능
-        cookie.setMaxAge((int) expirationMillis); // 쿠키 만료시간 설정
-        return cookie;
-    }
+    private final String COOKIE_NAME = "refresh_token";
 
     // 리프레쉬 토큰이 담긴 쿠키를 생성하는 메서드
-    public Cookie createFreshTokenCookie(ObjectId userId, long expirationMillis) {
+    public ResponseCookie createCookie(ObjectId userId, long expirationMillis) {
         String cookieValue = jwtUtil.generateRefreshToken(userId, expirationMillis);
-        Cookie cookie = new Cookie(FRESH_TOKEN_COOKIE, cookieValue);
-        // 쿠키 속성 설정
-        cookie.setHttpOnly(true);
-        cookie.setSecure(true);
-        cookie.setPath("/");
-        cookie.setMaxAge((int) expirationMillis);
-        return cookie;
+
+        return ResponseCookie.from(COOKIE_NAME, cookieValue)
+                .path("/")
+                .sameSite("None")
+                .httpOnly(true)
+                .secure(true)
+                .maxAge((int) expirationMillis)
+                .build();
     }
 
-    // 특정 쿠키를 찾아 반환하는 메서드
-    public Cookie getCookie(HttpServletRequest request, String cookieName) {
+    // 쿠키를 찾아 반환하는 메서드
+    public Cookie getCookie(HttpServletRequest request) {
         Cookie[] cookies = request.getCookies();
         if (cookies != null) {
             for (Cookie cookie : cookies) {
-                if (cookie.getName().equals(cookieName)) {
+                if (cookie.getName().equals(COOKIE_NAME)) {
                     return cookie;
                 }
             }
         }
+
         return null;
+    }
+
+    // 쿠키를 삭제하는 메서드
+    public Cookie deleteCookie() {
+        Cookie cookie = new Cookie(COOKIE_NAME, "");
+        cookie.setMaxAge(0); // 만료시간을 0으로 설정하여 삭제
+        cookie.setPath("/"); // 삭제되는 쿠키의 경로 설정
+        return cookie;
     }
 }
