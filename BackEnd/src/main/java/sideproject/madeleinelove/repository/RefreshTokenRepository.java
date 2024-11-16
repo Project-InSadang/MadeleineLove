9c218075fb2e524947505abce0f5b3f945e4fec4
susooo <92291198+susooo@users.noreply.github.com>
2024-11-16 22:36:35 +0900

    [fix] : refresh 토큰 예외를 Optional로 처리

--- BackEnd/src/main/java/sideproject/madeleinelove/repository/RefreshTokenRepository.java
+++ BackEnd/src/main/java/sideproject/madeleinelove/repository/RefreshTokenRepository.java
@@ -4,13 +4,17 @@ import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import sideproject.madeleinelove.entity.RefreshToken;
 
-@Repository
-public interface RefreshTokenRepository extends MongoRepository<RefreshToken, Long> {
-    @Query("{ 'userId': ?0 }")
-    RefreshToken findByUserId(ObjectId userId);
+import java.util.Optional;
 
+@Repository
+public interface RefreshTokenRepository extends MongoRepository<RefreshToken, ObjectId> {
+    @Query("{ 'userId': ?0 }")
+    Optional<RefreshToken> findByUserId(ObjectId userId);
+
+    @Transactional
     @Query(value = "{ 'userId': ?0 }", delete = true)
     void deleteByUserId(ObjectId userId);
 }
