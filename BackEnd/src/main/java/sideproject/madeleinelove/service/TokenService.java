54d62b1617a31454fbdfcc7c8226c32987f73094
susooo <92291198+susooo@users.noreply.github.com>
2024-11-16 23:11:00 +0900

    [refactor]: 인터페이스 메서드 반환 타입 변경

--- BackEnd/src/main/java/sideproject/madeleinelove/service/TokenService.java
+++ BackEnd/src/main/java/sideproject/madeleinelove/service/TokenService.java
@@ -2,7 +2,10 @@ package sideproject.madeleinelove.service;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Service;
+import sideproject.madeleinelove.dto.TokenDTO;
 
+@Service
 public interface TokenService {
-    void reissueAccessToken(HttpServletRequest request, HttpServletResponse response);
+    TokenDTO.TokenResponse reissueAccessToken(HttpServletRequest request, HttpServletResponse response);
 }
