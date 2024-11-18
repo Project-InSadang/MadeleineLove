afa8f5a2dde781990cb76f6929dcef1c5448c532
susooo <92291198+susooo@users.noreply.github.com>
2024-11-19 04:19:26 +0900

    [feat]: 모든 black postId만 조회하는 메서드 추가

--- BackEnd/src/main/java/sideproject/madeleinelove/repository/BlackPostRepository.java
+++ BackEnd/src/main/java/sideproject/madeleinelove/repository/BlackPostRepository.java
@@ -2,12 +2,17 @@ package sideproject.madeleinelove.repository;
 
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import sideproject.madeleinelove.dto.PostIdDTO;
 import sideproject.madeleinelove.entity.BlackPost;
+import sideproject.madeleinelove.entity.WhitePost;
 
 import java.util.List;
 
 public interface BlackPostRepository extends MongoRepository<BlackPost, ObjectId>, PostRepository {
-    List<BlackPost> findByUserId(String userId);
+    List<WhitePost> findByUserId(String userId);
 
-    long countLikesByPostId(ObjectId postId);
+    @Query(value = "{}", fields = "{ 'postId' : 1 }")
+    List<PostIdDTO> findAllPostIds();
+
 }
