e2f1e2423806e8a2e62417b2aebc02aafbdb3245
susooo <92291198+susooo@users.noreply.github.com>
2024-11-19 02:35:36 +0900

    [feat]: postId의 모든 userId를 조회하는 메서드 추가

--- BackEnd/src/main/java/sideproject/madeleinelove/repository/WhiteLikeRepository.java
+++ BackEnd/src/main/java/sideproject/madeleinelove/repository/WhiteLikeRepository.java
@@ -2,17 +2,19 @@ package sideproject.madeleinelove.repository;
 
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import sideproject.madeleinelove.dto.UserIdDTO;
 import sideproject.madeleinelove.entity.WhiteLike;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface WhiteLikeRepository extends MongoRepository<WhiteLike, ObjectId> {
-    List<WhiteLike> findByUserId(String userId);
 
     int countByPostId(ObjectId postId);
 
-    boolean existsByUserIdAndPostId(String userId, ObjectId postId);
-
     Optional<WhiteLike> findByUserIdAndPostId(String userId, ObjectId postId);
+
+    @Query(value = "{ 'postId' : ?0 }", fields = "{ 'userId' : 1 }")
+    List<UserIdDTO> findByPostId(ObjectId postId);
 }
