976b5bd5226e24a409c74e9dcef60347a5110d99
susooo <92291198+susooo@users.noreply.github.com>
2024-11-19 02:33:57 +0900

    [feat]: 모든 postId만 조회하는 메서드 추가

--- BackEnd/src/main/java/sideproject/madeleinelove/repository/WhitePostRepository.java
+++ BackEnd/src/main/java/sideproject/madeleinelove/repository/WhitePostRepository.java
@@ -2,6 +2,8 @@ package sideproject.madeleinelove.repository;
 
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import sideproject.madeleinelove.dto.PostIdDTO;
 import sideproject.madeleinelove.entity.WhitePost;
 
 import java.util.List;
@@ -9,6 +11,6 @@ import java.util.List;
 public interface WhitePostRepository extends MongoRepository<WhitePost, ObjectId>, PostRepository {
     List<WhitePost> findByUserId(String userId);
 
-    // 특정 게시물에 대한 좋아요 수 조회
-    long countLikesByPostId(ObjectId postId);
+    @Query(value = "{}", fields = "{ 'postId' : 1 }")
+    List<PostIdDTO> findAllPostIds();
 }
