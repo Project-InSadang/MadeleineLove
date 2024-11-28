8eff11223e66b3170411c6c95bf5aa9e91c9bf30
yxxngjxn <yxxngjxn99@gmail.com>
2024-11-28 14:46:32 +0900

    fix: RedisConfig 오류 수정

--- BackEnd/src/main/java/sideproject/madeleinelove/config/RedisConfig.java
+++ BackEnd/src/main/java/sideproject/madeleinelove/config/RedisConfig.java
@@ -3,6 +3,9 @@ package sideproject.madeleinelove.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
 public class RedisConfig {
@@ -20,8 +23,9 @@ public class RedisConfig {
 
     @Bean
     public RedisTemplate<?,?> redisTemplate() {
-        RedisTemplate<?,?> redisTemplate = new RedisTemplate<>();
+        RedisTemplate<?, ?> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory());
         return redisTemplate;
+    }
 
 }
