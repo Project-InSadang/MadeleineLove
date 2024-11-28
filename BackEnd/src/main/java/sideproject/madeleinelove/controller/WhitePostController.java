74b8a942818463cdd3102134f3e8b80c4bbc7e83
yxxngjxn <yxxngjxn99@gmail.com>
2024-11-28 16:02:31 +0900

    fix: WhitePostController import 수정

--- BackEnd/src/main/java/sideproject/madeleinelove/controller/WhitePostController.java
+++ BackEnd/src/main/java/sideproject/madeleinelove/controller/WhitePostController.java
@@ -1,6 +1,7 @@
 package sideproject.madeleinelove.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
