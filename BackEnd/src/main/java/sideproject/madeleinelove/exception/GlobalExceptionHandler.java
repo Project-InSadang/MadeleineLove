92427c0793f3fd5d1e647ed436bf32028eadb191
yxxngjxn <yxxngjxn99@gmail.com>
2024-12-19 14:43:39 +0900

    fix: @RestControllerAdvice 어노테이션 수정

--- BackEnd/src/main/java/sideproject/madeleinelove/exception/GlobalExceptionHandler.java
+++ BackEnd/src/main/java/sideproject/madeleinelove/exception/GlobalExceptionHandler.java
@@ -13,10 +13,11 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import sideproject.madeleinelove.base.ApiResponse;
 import sideproject.madeleinelove.base.BaseErrorCode;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
