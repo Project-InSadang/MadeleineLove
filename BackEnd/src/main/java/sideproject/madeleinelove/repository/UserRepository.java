3c0ceb9a1b9d9fb7266407bb4bf676dcc2608596
susooo <92291198+susooo@users.noreply.github.com>
2024-12-19 16:35:58 +0900

    [feat]: 회원탈퇴 (유저 삭제)

--- BackEnd/src/main/java/sideproject/madeleinelove/repository/UserRepository.java
+++ BackEnd/src/main/java/sideproject/madeleinelove/repository/UserRepository.java
@@ -5,9 +5,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import sideproject.madeleinelove.entity.User;
 
-public interface UserRepository extends MongoRepository<User, String> {
+public interface UserRepository extends MongoRepository<User, ObjectId> {
     User findByProviderId(String providerId);
 
-    @Query("{ 'userId': ?0 }")
-    User findByUserId(ObjectId userId);
 }
