416499b56f2c47805e2de1325805657ed137365a
yxxngjxn <yxxngjxn99@gmail.com>
2025-01-26 15:08:32 +0900

    feature: BlacklistTokenRepository 추가

---
+++ BackEnd/src/main/java/sideproject/madeleinelove/repository/BlacklistTokenRepository.java
@@ -0,0 +1,25 @@
+package sideproject.madeleinelove.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+
+
+@Component
+public class BlacklistTokenRepository {
+
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
+
+    private static final String BLACKLIST_PREFIX = "blacklist:";
+
+    public void save(String token, Duration expiration) {
+        redisTemplate.opsForValue().set(BLACKLIST_PREFIX + token, "true", expiration);
+    }
+
+    public boolean exists(String token) {
+        return Boolean.TRUE.equals(redisTemplate.hasKey(BLACKLIST_PREFIX + token));
+    }
+}
